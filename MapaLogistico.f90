82f5f753f52ea066c39f19a53b65e4025d2306f0
mberg1996 <40181353+mberg1996@users.noreply.github.com>
2018-06-15 17:58:21 +0000

    Update and rename mapa.f90 to MapaLogistico.f90

--- MapaLogistico.f90
+++ MapaLogistico.f90
@@ -16,7 +16,7 @@ PROGRAM Mapa_Logistico
     100 FORMAT (' ', i5, ' ', f20.15, f20.15, f20.15)
     OPEN(10, FILE = 'mapa.dat')
 
-	 WRITE(10,100) i, x, y, z
+	WRITE(10,100) i, x, y, z
 
 !ITERACAO!
 
