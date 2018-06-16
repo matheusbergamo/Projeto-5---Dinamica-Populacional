910ff611914abf1abe8673d53c5bcd385a88791c
mberg1996 <40181353+mberg1996@users.noreply.github.com>
2018-06-16 14:00:51 -0300

    Update and rename lya.f90 to Coeficiente_de_Lyapunov.f90

--- Coeficiente_de_Lyapunov.f90
+++ Coeficiente_de_Lyapunov.f90
@@ -5,15 +5,15 @@ PROGRAM Coeficiente_de_Lyapunov
 	!Constantes!
 	REAL(8), PARAMETER :: r = 2.5 !Taxa de crescimento!
     
-	!Razoes iniciais! 
-    REAL(8) :: x = 0.3d0 
+	!Razoes iniciais!
+	REAL(8) :: x = 0.3d0 
 
 	!Lyapunov!
 	REAL(8) :: lamb 
 
 !VARIAVEIS DE TRABALHO!
 
-    INTEGER :: i
+	INTEGER :: i
 	REAL(8) :: soma
 
 !CONDICOES INICIAIS!
@@ -23,15 +23,15 @@ PROGRAM Coeficiente_de_Lyapunov
 
 !FORMATACAO E OUTPUT!
 
-    100 FORMAT (' ', i5, ' ', f20.15, f20.15)
-    OPEN(10, FILE = 'mapalya.dat')
+	100 FORMAT (' ', i5, ' ', f20.15, f20.15)
+	OPEN(10, FILE = 'mapalya.dat')
 
 !ITERACAO!
 
-    DO i = 1, 100 !Geracoes!
-
-        !Recalculo das razoes!
-        x = r*x*(1 - x)
+	DO i = 1, 100 !Geracoes!
+	
+		!Recalculo das razoes!
+		x = r*x*(1 - x)
 
 		!Lyapunov!
 		lamb = LOG(ABS(r*(1 - 2*x)))
@@ -39,12 +39,12 @@ PROGRAM Coeficiente_de_Lyapunov
 		!Recalculo das somas!
 		soma = lamb + soma
 		
-        !Output!
-        WRITE(10,100) i, x, lamb
+		!Output!
+		WRITE(10,100) i, x, lamb
+		
+	END DO
 
-    END DO
-
-    CLOSE(10)
+	CLOSE(10)
 
 	WRITE(*,*) soma/(101)
 
